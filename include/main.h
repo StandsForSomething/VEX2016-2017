65936d33b78b89a2b7d7de691dfa4a5c992f0392
Josiah Atwell <1boy1dr@gmail.com>
2017-02-16 13:03:06 -0500

    tune arm #defines

--- include/main.h
+++ include/main.h
@@ -43,8 +43,8 @@
 // This prevents multiple inclusion, which isn't bad for this file but is good practice
 #define MAIN_H_
 
-#define ARM_LAUNCH_HEIGHT 2000
-#define ARM_MIN_HEIGHT 445
+#define ARM_LAUNCH_HEIGHT 2300
+#define ARM_MIN_HEIGHT 450
 #define ARM_CONST_POWER_HEIGHT_MAX 800
 #define ARM_CONST_POWER_HEIGHT_MIN 500
 #define ARM_CONST_POWER 15
